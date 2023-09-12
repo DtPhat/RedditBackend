22b8a87605bee6b49972b1a73b9a652eb16a5fc9
DtPhat <dtphatkh@gmail.com>
2023-09-12 18:55:43 +0700

    Add project files.

---
+++ Blueddit/Controllers/PostController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blueddit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("Hello");
+        }
+    }
+}
---
+++ Blueddit/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Blueddit.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
